aef117de8a88896c8c68a1be48e016a7007f9efe
doremon0710 <doremon0710@gmail.com>
2019-03-17 13:48:37 -0700

    Update index.js
    update index.js file for home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import Header from '../Header/header';
+import Footer from '../Footer';
 
 const Home = () => (
+  <Container>
   <div>
     <h1>Home Page</h1>
     <p> The Home Page is accessible by every user.</p>
   </div>
+  <Footer />
+  </Container>
 );
 
 export default Home;
